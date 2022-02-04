389dd1997653f08bb2c1995d59da851503c1ac60
netonblpb <skullbog@gmail.com>
2022-02-04 10:31:18 +0300

    Task11 complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Task11: Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Random rnd = new Random();
+int number = rnd.Next(10, 100);
+Console.WriteLine(number);
+
+int second = number % 10;
+int first = number / 10;
+
+if (first > second) Console.WriteLine("Первая цифра " + first + " больше второй " + second);
+else Console.WriteLine("Вторая цифра " + second + " больше первой " + first);
+
